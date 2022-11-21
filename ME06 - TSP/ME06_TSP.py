64f3675aea1636e17c0ff48f1eda46707b047a15
Marc Peejay Viernes <mpvviernes@gmail.com>
2022-11-21 19:44:12 +0800

    Add ME6: Travelling Salesman Problem (Perfect Score)

--- ME06 - TSP/ME06_TSP.py
+++ ME06 - TSP/ME06_TSP.py
@@ -11,8 +11,6 @@ def main():
     for _ in range(num_of_activities):
         activities.append(input().strip().split())
 
-    print(activities)
-
     # Build the complete edge-weighted graph.
     graph = []
     for i in range(num_of_activities):
@@ -28,53 +26,56 @@ def main():
 
         graph.append(temp_row)
 
-    print(graph)
-
     s = 0
     min_path = solve_travelling_salesman_problem(graph, s, num_of_activities)
-    print(min_path)
 
     print(round(min_path[0], 3))
-
     print(activities[0][0])
+
     for activity_index in min_path[1]:
         print(activities[activity_index][0])
 
     print(activities[0][0])
 
-def solve_travelling_salesman_problem(graph, s, no_of_activities):
-    # store all vertex apart from source vertex
-    vertex = []
+
+def solve_travelling_salesman_problem(graph, starting_node, no_of_activities):
+    # Determine the all the nodes (except for the start node).
+    nodes = []
     for i in range(no_of_activities):
-        if i != s:
-            vertex.append(i)
-    print(vertex)
+        if i != starting_node:
+            nodes.append(i)
 
-    # store minimum weight Hamiltonian Cycle
-    min_path = maxsize
-    next_permutation = permutations(vertex)
-    # list_permutations = list(next_permutation)
-    # print(list_permutations)
+    # Find minimum weight (Hamiltonian cycle).
+    prev_min_path_weight = None
+    curr_min_path_weight = maxsize
+    min_path = ()
+    iter_permutations = permutations(nodes)
 
-    for i in next_permutation:
-        # store current Path weight(cost)
-        current_pathweight = 0
+    for i in iter_permutations:
+        curr_path_weight = 0
 
-        # compute current path weight
-        k = s
+        k = starting_node
         for j in i:
-            current_pathweight += graph[k][j]
+            curr_path_weight += graph[k][j]
             k = j
-        current_pathweight += graph[k][s]
+        curr_path_weight += graph[k][starting_node]
 
-        # update minimum
-        min_path = min(min_path, current_pathweight)
+        # Update the minimum weight (with partial Hamiltonian path).
+        curr_min_path_weight = min(curr_min_path_weight, curr_path_weight)
 
-    return min_path, i
+        if prev_min_path_weight is None:
+            prev_min_path_weight = curr_min_path_weight
+            min_path = i
+
+        if curr_min_path_weight < prev_min_path_weight:
+            prev_min_path_weight = curr_min_path_weight
+            min_path = i
+
+    return curr_min_path_weight, min_path
 
 
 def get_distance(pos1, pos2):
-    return round(math.sqrt(((pos1[0] - pos2[0]) ** 2 + (pos1[1] - pos2[1]) ** 2)) * 1000, 3)
+    return math.sqrt(((pos1[0] - pos2[0]) ** 2 + (pos1[1] - pos2[1]) ** 2)) * 1000
 
 
 if __name__ == '__main__':
